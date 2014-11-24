c8e10b66bda446ff9aa3eee9526d4e2a16c7d90c
Emily Plummer <eplummer26@gmail.com>
2014-11-23 17:56:28 -0800

    keel buttons

---
+++ version-two/home.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+$( ".drop-button-1" ).click(function() {
+  $( ".experience-work-1" ).toggle( "slow", function() {
+  });
+});
+});
+
+$(document).ready(function() {
+$( ".drop-button-2" ).click(function() {
+  $( ".experience-work-2" ).toggle( "slow", function() {
+  	$(".experience-work-2").css("visibility", "visible");
+  });
+});
+});
+
+
+$(document).ready(function() {
+$( ".drop-button-3" ).click(function() {
+  $( ".experience-work-3" ).toggle( "slow", function() {
+  	$(".experience-work-3").css("visibility", "visible");
+  });
+});
+});
